5df85b2f300b547080ab28aa3f470facf430d561
wasiwasi <kbm4250@naver.com>
2022-09-18 16:27:03 +0900

    feat: Impl change rtsp stream to websocket stream

--- Rtsp2img.js
+++ Rtsp2img.js
@@ -1,11 +1,30 @@
+const Stream = require("node-rtsp-stream");
+
 module.exports = function (RED) {
   function Rtsp2imgNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on("input", function (msg) {
-      msg.payload = msg.payload;
-      node.send(msg);
+      const rtspURL = config.rtspURL;
+      const smartthingsMnid = this.credentials.smartthingsMnid;
+      const smartthingsPat = this.credentials.smartthingsPat;
+
+      stream = new Stream({
+        name: "name",
+        streamUrl: `rtsps://${smartthingsMnid}:${smartthingsPat}@${rtspURL}`,
+        wsPort: 9999,
+        ffmpegOptions: {
+          // options ffmpeg flags
+          "-stats": "", // an option with no neccessary value uses a blank string
+          "-r": 30, // options with required values specify the value after the key
+        },
+      });
     });
   }
-  RED.nodes.registerType("rtsp2img", Rtsp2imgNode);
+  RED.nodes.registerType("rtsp2img", Rtsp2imgNode, {
+    credentials: {
+      smartthingsMnid: { value: "" },
+      smartthingsPat: { value: "" },
+    },
+  });
 };
